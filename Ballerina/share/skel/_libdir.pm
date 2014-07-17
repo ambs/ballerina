02f5a25193c93513c1488b1b68df44f0f51acf0e
ambs <ambs@cpan.org>
2014-07-17 20:53:31 +0100

    Listar tabelas no navbar

--- Ballerina/share/skel/_libdir.pm
+++ Ballerina/share/skel/_libdir.pm
@@ -2,6 +2,11 @@ package [% appname %];
 use Dancer2;
 use Dancer2::Plugin::DBIC;
 
+hook before_template_render => sub {
+    my $tokens = shift;
+    $tokens->{tables} = [schema->sources];
+};
+
 get '/' => sub {
     template 'index';
 };
