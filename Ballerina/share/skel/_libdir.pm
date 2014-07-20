a1248763b4a0edcad72160e36021a68167f3ac13
ambs <ambs@cpan.org>
2014-07-20 21:29:52 +0100

    list records

--- Ballerina/share/skel/_libdir.pm
+++ Ballerina/share/skel/_libdir.pm
@@ -3,8 +3,7 @@ use Dancer2;
 use Dancer2::Plugin::DBIC;
 use YAML::Tiny;
 
-our $_SCHEMA = YAML::Tiny->read("database.yml");
-
+our $_SCHEMA = YAML::Tiny->read("database.yml")->[0];
 
 hook before_template_render => sub {
     my $tokens = shift;
@@ -17,10 +16,31 @@ get '/' => sub {
 
 [% FOREACH table IN tables %]
 get '/[% table.name %]' => sub {
-	my $table = { name => "[% table.name %]" };
+	my $table = "[% table.name %]";
+	my $table_info = {
+		name    => $table,
+	    columns => [ sort {
+	    	$_SCHEMA->{$table}{$a}{order} <=> $_SCHEMA->{$table}{$b}{order}
+	    } keys %{$_SCHEMA->{$table}} ],
+	};
 
 	template 'table' => {
-		table => $table,
+		table => $table_info,
+		rows  => [ schema->resultset($table)->search( undef, { rows => 1000 })],
+	};
+};
+
+get '/[% table.name %]/new' => sub {
+	my $table = "[% table.name %]";
+	my $table_info = {
+		name    => $table,
+	    columns => [ sort {
+	    	$_SCHEMA->{$table}{$a}{order} <=> $_SCHEMA->{$table}{$b}{order}
+	    } keys %{$_SCHEMA->{$table}} ],
+	};
+
+	template 'new_record' => {
+		table => $table_info,
 	};
 };
 [% END %]
