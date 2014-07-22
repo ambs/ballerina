cb9184862089e813a1fc53e274daa1f7485a9201
ambs <ambs@cpan.org>
2014-07-22 15:12:27 +0100

    New record

--- Ballerina/share/skel/_libdir.pm
+++ Ballerina/share/skel/_libdir.pm
@@ -1,7 +1,9 @@
 package [% appname %];
 use Dancer2;
 use Dancer2::Plugin::DBIC;
+use Dancer2::Plugin::Deferred;
 use YAML::Tiny;
+use Try::Tiny;
 
 our $_SCHEMA = YAML::Tiny->read("database.yml")->[0];
 
@@ -47,13 +49,25 @@ get '/[% table.name %]/new' => sub {
 };
 
 post '/[% table.name %]' => sub {
+	my $table  = "[% table.name %]";	
 	my $action = param('action') || "NOOP";
 
-	if ($action eq "new") {
+	if ($action eq "new") {		
+		## XXX - For now, not testing if all is filled up
+		
+		my %record = map  { ($_ => param "input_$_" ) }
+					 map  { s/^input_//; $_ }
+					 grep { m!^input_! }
+					 keys params();
 
+		schema->resultset($table)->create({%record});
+		deferred type    => 'success';
+		deferred message => "Record added successfully";
+		## XXX - later check if there was an error.
+		redirect "/$table";
 	}
 	else {
-		forward '/[% table.name %]', { }, { method => 'GET' };
+		forward "/$table", { }, { method => 'GET' };
 	}
 };
 [% END %]
